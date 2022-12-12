cc9614bf8aeedc7b6be64d1cf8020e97cabf62a2
Hossein <djalicrt@gmail.com>
2022-12-12 11:30:50 +0330

    Add Some Bootsrap Stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Container , Button , Card , InputGroup , Row, FormControl} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <InputGroup className='mb-3' size='lg'>
+          <FormControl
+            placeholder='Search Artist'
+            type='input'
+            onKeyPress={event => {
+              if (event.key == "Enter") {
+                console.log("Pressed Enter")
+              }
+            }} ></FormControl>            
+
+          </InputGroup>
+        </Container>
     </div>
   );
 }
